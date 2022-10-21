4754c17fb1f3a19fa95ae70991e6fabd7aa65a18
Franco Pajello <francoagustinpajello@gmail.com>
2022-10-21 01:21:04 -0300

    segundo desafio fs

--- index.js
+++ index.js
@@ -1,58 +1,137 @@
-class Usuario {
-    constructor(nombre, apellido, libros, mascotas) {
+const fs = require("fs");
+const data = './data/data.json';
+
+class productos {
+    constructor(nombre) {
         this.nombre = nombre
-        this.apellido = apellido
-        this.libros = [{
-            nombre: libros,
-            autor: undefined
-        }]
-        this.mascotas = [mascotas]
-    }
-    getFullName() {
-
-        console.log(`${this.nombre} ${this.apellido}`)
-
-    }
-    addMascota(mascota) {
-
-        this.mascotas.push(mascota)
-
-        return console.log(this.mascotas)
 
     }
 
-    countMascotas() {
+    async save(obj) {
 
-        console.log(this.mascotas.length)
+        try {
+            const lecturaArchivo = await fs.promises.readFile(data, "utf-8");
+
+            const archivoFormatoJs = JSON.parse(lecturaArchivo);
+
+
+            if (archivoFormatoJs.length == 0) {
+
+                obj.id = 1;
+                archivoFormatoJs.push(obj);
+
+                let archivoFormatoTxt = JSON.stringify(archivoFormatoJs);
+
+                await fs.promises.writeFile(data, archivoFormatoTxt);
+
+                return console.log(obj.id);
+
+            }
+
+            else {
+
+                let arraryId = archivoFormatoJs.map(e => e.id);
+
+                let sumandoId = Math.max(...arraryId);
+
+                obj.id = sumandoId + 1;
+
+                archivoFormatoJs.push(obj);
+
+                let archivoFormatoTxt = JSON.stringify(archivoFormatoJs);
+
+                await fs.promises.writeFile(data, archivoFormatoTxt);
+
+                return console.log(obj.id);
+            }
+        }
+        catch (e) {
+            console.log(e);
+
+        }
+
     }
 
-    addBook(nombre, autor) {
+    async getById(id) {
+        try {
 
-        this.libros.push({
-            nombre,
-            autor
-        })
+            const lecturaArchivo = await fs.promises.readFile(data, "utf-8");
 
-        return console.log(this.libros)
+            const archivoFormatoJs = JSON.parse(lecturaArchivo);
 
+            let buscandoId = archivoFormatoJs.filter(e => e.id === id | null);
+
+            console.log(buscandoId);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    getBookNames() {
+    async getAll() {
+        try {
 
-        let nombresLibros = this.libros.map((e) => e.nombre)
-        return console.log(nombresLibros)
+            const lecturaArchivo = await fs.promises.readFile(data, "utf-8");
 
+            const archivoFormatoJs = JSON.parse(lecturaArchivo);
+
+            console.log(archivoFormatoJs);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async deleteById(id) {
+        try {
+            const lecturaArchivo = await fs.promises.readFile(data, "utf-8");
+
+            const archivoFormatoJs = JSON.parse(lecturaArchivo);
+
+            const buscamosElementoId = archivoFormatoJs.find((e) => e.id === id);
+
+            const indice = archivoFormatoJs.indexOf(buscamosElementoId);
+
+            await archivoFormatoJs.splice(indice, 1);
+
+            let archivoFormatoTxt = JSON.stringify(archivoFormatoJs);
+
+            await fs.promises.writeFile('./data/data.json', archivoFormatoTxt);
+
+            return console.log(archivoFormatoJs);
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+    async deleteAll() {
+        try {
+
+            const lecturaArchivo = await fs.promises.readFile(data, "utf-8");
+            let archivoFormatoJs = JSON.parse(lecturaArchivo);
+            archivoFormatoJs = [];
+            let archivoFormatoTxt = JSON.stringify(archivoFormatoJs);
+            await fs.promises.writeFile('./data/data.json', archivoFormatoTxt);
+            return console.log(archivoFormatoJs);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
-const obj = new Usuario("Franco", "Pajello", "El señor de los anillos", "gato")
+const objeto = new productos(data)
 
-console.log(obj.nombre)
-console.log(obj.apellido)
-console.log(obj.libros)
-console.log(obj.mascotas)
-obj.getFullName()
-obj.addMascota("perro")
-obj.countMascotas()
-obj.addBook("It", "Stephen King")
-obj.getBookNames() 
+objeto.save({
+    id: null,
+    producto: "galletitas",
+    precio: 200
+});
+objeto.getById(7);
+
+objeto.deleteById(2);
+
+objeto.getAll();
+
+setTimeout(() => {
+
+    objeto.deleteAll();
+}, 0);
+
