fd60fe64e0ef264a7b0f099096137a27290180f2
Franco Pajello <francoagustinpajello@gmail.com>
2023-02-21 21:51:01 -0300

    deploy a reway 0

--- NginxNode/consoleLog.js
+++
@@ -1,9 +0,0 @@
-const { fork } = require("child_process")
-
-process.on("message", (msg) => {
-    let a = ""
-    if (msg == "start") {
-        a = "A"
-    }
-    process.send({ type: "listo", data: a })
-})
